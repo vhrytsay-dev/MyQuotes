1a39112870a7c6f2a7317536ea46c3f89d7f45ed
Viktor Hrytsay <viktor2@uni-bremen.de>
2022-04-19 20:27:14 +0200

    Add DataBinding & LiveData example

--- app/src/main/java/com/example/myquotes/MainActivity.kt
+++ app/src/main/java/com/example/myquotes/MainActivity.kt
@@ -2,68 +2,17 @@ package com.example.myquotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.myquotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var model: QuoteViewModel
 
-    private val quotes = listOf(
-        Quote(
-            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entsranden sind.",
-            "Albert Einstein",
-            "1948"
-        ),
-        Quote(
-            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
-            "Marie Curie",
-            "1928"
-        ),
-        Quote(
-            "Nichts ist so beständig wie der Wandel",
-            "Heraklit",
-            "480 v. Chr."
-        )
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        model = ViewModelProvider(this).get(QuoteViewModel::class.java)
-        showQuote()
-        setVisibilityOfButtons()
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = ViewModelProvider(this).get(QuoteViewModel::class.java)
+        binding.lifecycleOwner = this
     }
-
-    fun nextQuote(view: View) {
-        if(model.index < quotes.size -1) model.index++
-        showQuote()
-        setVisibilityOfButtons()
-    }
-
-    fun previousQuote(view: View) {
-        if(model.index > 0) model.index--
-        showQuote()
-        setVisibilityOfButtons()
-    }
-    private fun showQuote() {
-        binding.quote = quotes[model.index]
-    }
-
-    private fun setVisibilityOfButtons() = when (model.index) {
-            0 -> binding.apply{
-                quotePrevious.visibility = View.INVISIBLE
-                quoteNext.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
-            }
-            quotes.size - 1 -> binding.apply{
-                quoteNext.visibility = View.INVISIBLE
-                quotePrevious.visibility = if(quotes.size > 1) View.VISIBLE else View.INVISIBLE
-            }
-            else -> binding.apply{
-                quoteNext.visibility = View.VISIBLE
-                quotePrevious.visibility = View.VISIBLE
-            }
-        }
 }
--- app/src/main/java/com/example/myquotes/QuoteViewModel.kt
+++ app/src/main/java/com/example/myquotes/QuoteViewModel.kt
@@ -1,7 +1,48 @@
 package com.example.myquotes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class QuoteViewModel : ViewModel() {
-    var index = 0
+    val quotes = listOf(
+        Quote(
+            "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entsranden sind.",
+            "Albert Einstein",
+            "1948"
+        ),
+        Quote(
+            "Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.",
+            "Marie Curie",
+            "1928"
+        ),
+        Quote(
+            "Nichts ist so beständig wie der Wandel",
+            "Heraklit",
+            "480 v. Chr."
+        )
+    )
+
+    private var index = 0
+    private val _quote = MutableLiveData<Quote>().apply { value = quotes[index] }
+    val quote: LiveData<Quote>
+        get() = _quote
+    val isFirst = Transformations.map(quote) {index == 0}
+    val isLast = Transformations.map(quote){index == quotes.size - 1}
+
+    fun nextQuote() {
+        if(index < quotes.size -1) {
+            index++
+            _quote.value = quotes[index]
+        }
+    }
+
+    fun previousQuote() {
+        if(index > 0) {
+            index--
+            _quote.value = quotes[index]
+        }
+
+    }
 }
